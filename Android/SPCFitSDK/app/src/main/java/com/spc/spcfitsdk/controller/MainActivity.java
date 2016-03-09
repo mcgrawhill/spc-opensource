5702db607d4348df73c05a2557638efb7f24319a
Gonzalo <hilgroth@gmail.com>
2016-03-09 09:26:13 +0100

    Convertir en librería el proyecto Android

--- Android/SPCFitSDK/app/src/main/java/com/spc/spcfitsdk/controller/MainActivity.java
+++ Android/SPCFitSDK/app/src/main/java/com/spc/spcfitsdk/controller/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.spc_activity_main);
 
         exampleManager = ExampleManager.getInstance(getApplicationContext());
 
